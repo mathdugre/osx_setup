1d8c21217208e3e08929d4231d898504603c4bc6
Mathieu Dugré <math.dugre@gmail.com>
2019-10-11 10:39:00 -0400

    Deleted default osx app

---
+++ del_default_app.sh
@@ -0,0 +1,23 @@
+sudo rm -rf /Applications/Books.app
+sudo rm -rf /Applications/Calendar.app
+sudo rm -rf /Applications/Chess.app
+sudo rm -rf /Applications/Contact.app
+sudo rm -rf /Applications/Dashboard.app
+sudo rm -rf /Applications/Dictionary.app
+sudo rm -rf /Applications/Facetime.app
+sudo rm -rf /Applications/Home.app
+sudo rm -rf /Applications/Image\ Capture.app
+sudo rm -rf /Applications/iTunes.app
+sudo rm -rf /Applications/Launchpad.app
+sudo rm -rf /Applications/Maps.app
+sudo rm -rf /Applications/Messages.app
+sudo rm -rf /Applications/News.app
+sudo rm -rf /Applications/Note.app
+sudo rm -rf /Applications/Photo\ Booth.app
+sudo rm -rf /Applications/Photos.app
+sudo rm -rf /Applications/Reminders.app
+sudo rm -rf /Applications/Safari.app
+sudo rm -rf /Applications/Siri.app
+sudo rm -rf /Applications/Stickies.app
+sudo rm -rf /Applications/Stocks.app
+sudo rm -rf /Applications/VoiceMemos.app
